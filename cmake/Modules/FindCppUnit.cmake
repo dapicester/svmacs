9d8d412eebe88a61f6f79f038a7241ed39c02eab
paolo <paolo@744eb53d-dda4-4b2a-8815-b91074537ad3>
2011-03-02 04:05:52 +0000

    setup tests

---
+++ cmake/Modules/FindCppUnit.cmake
@@ -0,0 +1,28 @@
+# - Try to find CppUnit
+# Once done this will define
+#  CPPUNIT_FOUND - System has CppUnit
+#  CPPUNIT_INCLUDE_DIRS - The CppUnit include directories
+#  CPPUNIT_LIBRARIES - The libraries needed to use CppUnit
+#  CPPUNIT_DEFINITIONS - Compiler switches required for using CppUnit
+
+find_package(PkgConfig)
+pkg_check_modules(PC_CPPUNIT QUIET CPPUNIT)
+set(CPPUNIT_DEFINITIONS ${PC_CPPUNIT_CFLAGS_OTHER})
+
+find_path(CPPUNIT_INCLUDE_DIR cppunit/Test.h
+          HINTS ${PC_CPPUNIT_INCLUDEDIR} ${PC_CPPUNIT_INCLUDE_DIRS}
+          PATH_SUFFIXES CPPUNIT )
+
+find_library(CPPUNIT_LIBRARY NAMES cppunit libccpunit
+             HINTS ${PC_CPPUNIT_LIBDIR} ${PC_CppUnit_LIBRARY_DIRS} )
+
+set(CppUnit_LIBRARIES ${CPPUNIT_LIBRARY} )
+set(CPPUNIT_INCLUDE_DIRS ${CPPUNIT_INCLUDE_DIR} )
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set CPPUNIT_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(CppUnit  DEFAULT_MSG
+                                  CPPUNIT_LIBRARY CPPUNIT_INCLUDE_DIR)
+
+mark_as_advanced(CPPUNIT_INCLUDE_DIR CPPUNIT_LIBRARY )
