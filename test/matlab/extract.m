1d790fc5c5849fba4bb8d6194bf5dd2b852ccef1
Paolo D'Apice <dapicester@gmail.com>
2011-04-08 13:10:27 +0800

    Matlab invece di Octave
    Aggiunto il codice per l'estrazione delle features hr ed mfcc con Matlab;
    incompatibilità Octave/Matlab

--- test/matlab/extract.m
+++ test/matlab/extract.m
@@ -1,21 +1,30 @@
-printf("loading ...\n");
-itload("signal.it");
+clc;
+clear all;
+fprintf('loading ...\n');
+itload('signal.it');
 
-printf("signal ...\n");
-featuresSignal = zeros(1,5);
+wts = generateMelFilters(nfft-1,sampleRate,24);
+wts = wts(:,1:nfft/2);
+
+fprintf('signal ...\n');
+featuresSignal = zeros(1,12);
 featuresSignal(1) = zcr(signal, sampleRate);
 featuresSignal(2) = energy(signal, sampleRate);
 [featuresSignal(3), featuresSignal(4)] = ass(signalSpectrum);
 featuresSignal(5) = srf(signalSpectrum);
+featuresSignal(6) = hr(signal);
+featuresSignal(7:end) = mfcc(signalSpectrum, 6, wts, 24); 
 
-printf("silence ...\n");
-featuresSilence = zeros(1,5);
+fprintf('silence ...\n');
+featuresSilence = zeros(1,12);
 featuresSilence(1) = zcr(silence, sampleRate);
 featuresSilence(2) = energy(silence, sampleRate);
 [featuresSilence(3), featuresSilence(4)] = ass(silenceSpectrum);
 featuresSilence(5) = srf(silenceSpectrum);
+featuresSilence(6) = hr(silence);
+featuresSilence(7:end) = mfcc(silenceSpectrum, 6, wts, 24);
 
-printf("saving...\n");
-itsave("matlab.it", [featuresSignal], [featuresSilence]);
+fprintf('saving...\n');
+itsave('matlab.it', [featuresSignal], [featuresSilence]);
 
-printf("DONE.\n\n");
+fprintf('DONE.\n\n');
