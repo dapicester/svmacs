358a9887da36b8da4d79659db5f2e048b769f5d7
Paolo D'Apice <dapicester@gmail.com>
2011-07-12 17:15:43 +0800

    Revert "aggiornato .gitignore"
    This reverts commit 0743ebe8f68d05ea97618733c0d6f827022c174b.

---
+++ test/utils/path.h
@@ -0,0 +1,21 @@
+/*
+ * File:   path.h
+ * Author: paolo
+ *
+ * Created on April 2, 2011, 5:22 PM
+ */
+
+#ifndef PATH_H
+#define	PATH_H
+
+#include <string>
+
+/*
+ * FIXME: trovare una soluzione alternativa
+ * TODO: usare cmakedefine
+ */
+
+static const std::string PATH = "/home/paolo/NetBeansProjects/svmacs/";
+
+#endif	/* PATH_H */
+
