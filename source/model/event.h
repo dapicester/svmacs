24ce2b2470d8317b96fcaaa1c9019cb76d0da8b9
Paolo D'Apice <paolo.dapice@izenesoft.com>
2012-06-27 16:37:57 +0800

    Implementazione di Event in header

--- source/model/event.h
+++ source/model/event.h
@@ -11,31 +11,49 @@
 
 NS_SVMACS_BEGIN
 
-/** 
+/**
  * Type of detected event.
- */    
+ */
 enum EventType {
-    NONE, GUNSHOT, SCREAM, GLASS
+    NONE,       ///< No Event
+    GUNSHOT,    ///< Gunshot
+    SCREAM,     ///< Scream
+    GLASS       ///< Breaking glass
 };
 
 /**
  * Detected event.
  */
-class Event { 
+class Event {
 public:
-    Event();
-    Event(const EventType& t, const std::string& desc = "");
-    Event(const Event& e);
-    ~Event();
-    
+    /// Constructor.
+    Event(const EventType t = NONE, const std::string& desc = "")
+        : type(t), description(desc) {}
+
+    /// Copy constructor.
+    Event(const Event& e)
+        : type(e.getType()), description(e.getDescription()) {}
+
+    /// Destructor.
+    ~Event() {}
+
     /// Getter for the type property.
-    EventType getType() const;
-    
+    EventType getType() const {
+        return type;
+    }
+
     /// Getter for the description property
-    std::string getDescription() const ;
-    
-private: // TODO: add details, e.g. timestamp
+    std::string getDescription() const {
+        return description;
+    }
+
+private:
+
+    /// The event type.
     EventType type;
+
+    /// A descriptive string.
+    // TODO: add details, e.g. timestamp
     const std::string description;
 };
 
